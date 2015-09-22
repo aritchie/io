b2399ee28f773634195d21bb7d2ed042332b1b26
Allan Ritchie <allan.ritchie@gmail.com>
2015-09-22 09:29:54 -0400

    NET45 implementation

---
+++ src/Acr.IO.NetCore/FileViewerImpl.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Diagnostics;
+
+
+namespace Acr.IO {
+
+    public class FileViewerImpl : IFileViewer {
+
+        public bool Open(IFile file) {
+            var process = Process.Start(file.FullName);
+            return process != null;
+        }
+    }
+}
--- src/Acr.IO.WindowsPhone/FileSystemImpl.cs
+++ src/Acr.IO.WindowsPhone/FileSystemImpl.cs
@@ -1,39 +1,39 @@
-﻿using System;
+﻿//using System;
 
 
-namespace Acr.IO {
+//namespace Acr.IO {
 
-    public class FileSystemImpl : IFileSystem {
-        public IDirectory AppData {
-            get {
-                throw new NotImplementedException();
-            }
-        }
+//    public class FileSystemImpl : IFileSystem {
+//        public IDirectory AppData {
+//            get {
+//                throw new NotImplementedException();
+//            }
+//        }
 
-        public IDirectory Cache {
-            get {
-                throw new NotImplementedException();
-            }
-        }
+//        public IDirectory Cache {
+//            get {
+//                throw new NotImplementedException();
+//            }
+//        }
 
-        public IDirectory Public {
-            get {
-                throw new NotImplementedException();
-            }
-        }
+//        public IDirectory Public {
+//            get {
+//                throw new NotImplementedException();
+//            }
+//        }
 
-        public IDirectory Temp {
-            get {
-                throw new NotImplementedException();
-            }
-        }
+//        public IDirectory Temp {
+//            get {
+//                throw new NotImplementedException();
+//            }
+//        }
 
-        public IDirectory GetDirectory(string path) {
-            throw new NotImplementedException();
-        }
+//        public IDirectory GetDirectory(string path) {
+//            throw new NotImplementedException();
+//        }
 
-        public IFile GetFile(string path) {
-            throw new NotImplementedException();
-        }
-    }
-}
+//        public IFile GetFile(string path) {
+//            throw new NotImplementedException();
+//        }
+//    }
+//}
